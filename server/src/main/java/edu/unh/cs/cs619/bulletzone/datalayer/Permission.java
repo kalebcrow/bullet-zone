29c57dc23beae60d4f9d74ea1ba1d79c3fc5d64b
Matthew Plumlee <mdp@ccom.unh.edu>
2021-01-21 15:52:12 -0500

    Added convenience functions to Permission enum (not tested yet).

--- server/src/main/java/edu/unh/cs/cs619/bulletzone/datalayer/Permission.java
+++ server/src/main/java/edu/unh/cs/cs619/bulletzone/datalayer/Permission.java
@@ -4,6 +4,64 @@
  */
 package edu.unh.cs.cs619.bulletzone.datalayer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Permission {
     Invalid, Owner, Add, Remove, Use, Transfer;
+
+    //Thanks to Jason Waleryszak for initial version of the convenience methods below
+
+    /**
+     * Get all permissions. Useful for replying this structure using json.
+     *
+     * @return Map of Permission id-Permission name pairs.
+     */
+    public static Map<Integer, String> getPermissionMapping() { return Helper.permissions; }
+
+    /**
+     * Get a Permission given its integer ID.
+     *
+     * @param permissionID ID of Permission.
+     * @return Permission associated with the ID. Permission.Invalid is returned for values
+     * out-of-range
+     */
+    public static Permission get(int permissionID) {
+        if (permissionID < 0 || permissionID >= values().length)
+            return Invalid;
+
+        return values()[permissionID];
+    }
+
+    /** @return the integer ID of the passed permission */
+    public static int getID(Permission p) {
+        return p.ordinal();
+    }
+
+    /**
+     * Get a Permission given its name.
+     *
+     * @param permissionName Name of Permission.
+     * @return Permission associated with the name. Permission.Invalid returned if no permission
+     * name matches.
+     */
+    public static Permission get(String permissionName) {
+        Permission result;
+
+        try {
+            result = Permission.valueOf(permissionName);
+        } catch (IllegalArgumentException e) {
+            result = Invalid;
+        }
+        return result;
+    }
+
+    //using a static class to get around the prohibition on touching static member from constructor
+    private static class Helper {
+        public static Map<Integer, String> permissions = new HashMap<>();
+    }
+
+    private Permission() {
+        Helper.permissions.put(ordinal(), name());
+    }
 }
