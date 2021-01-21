18a8b400184d943c1940d7df3e0ce6324d2f5404
Matthew Plumlee <mdp@ccom.unh.edu>
2021-01-21 16:43:51 -0500

    Added tests for Permission convenience functions.

---
+++ server/src/test/java/edu/unh/cs/cs619/bulletzone/datalayer/PermissionTest.java
@@ -0,0 +1,24 @@
+package edu.unh.cs.cs619.bulletzone.datalayer;
+
+import org.junit.Test;
+
+import java.util.Map;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class PermissionTest {
+
+    @Test
+    public void getPermissionMapping_usingIntegerGet_returnsConsistentMap() {
+        Map<Integer, String> m = Permission.getPermissionMapping();
+        for (Permission p : Permission.values())
+            assertThat(m.get(p.ordinal()), is(p.name()));
+    }
+
+    @Test
+    public void get_usingStringGet_returnsConsistentValues() {
+        for (Permission p : Permission.values())
+            assertThat(Permission.get(p.name()), is (p));
+    }
+}
