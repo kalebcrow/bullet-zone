8af02039955b693796f41336ed0deed34e3da9d0
cgladu <cgladu@iol.unh.edu>
2022-10-20 18:50:23 -0400

    Back to OG server

--- client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/BulletZoneRestClient.java
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/BulletZoneRestClient.java
@@ -23,8 +23,8 @@ import edu.unh.cs.cs619.bulletzone.util.LongWrapper;
  * Created by simon on 10/1/14.
  */
 
-@Rest(rootUrl = "http://10.21.159.62:8080/games",
-//@Rest(rootUrl = "http://stman1.cs.unh.edu:6192/games",
+//@Rest(rootUrl = "http://10.21.159.62:8080/games",
+@Rest(rootUrl = "http://stman1.cs.unh.edu:6192/games",
 //@Rest(rootUrl = "http://stman1.cs.unh.edu:61902/games",
         converters = {StringHttpMessageConverter.class, MappingJackson2HttpMessageConverter.class}
         // TODO: disable intercepting and logging
