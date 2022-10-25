0cffc2e25c7f21d059a40744539ba4d6c6c72ad6
Zachary Simone <zsimone@iol.unh.edu>
2022-10-24 20:48:34 -0400

    Merged with master and fixed merge conflicts

---
+++ server/src/test/java/edu/unh/cs/cs619/bulletzone/model/TankControllerTest.java
@@ -0,0 +1,31 @@
+package edu.unh.cs.cs619.bulletzone.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+import edu.unh.cs.cs619.bulletzone.model.TankController;
+import edu.unh.cs.cs619.bulletzone.BulletZoneServer;
+
+class TankControllerTest {
+
+    @Test
+    void turn() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void fire() {
+    }
+}
