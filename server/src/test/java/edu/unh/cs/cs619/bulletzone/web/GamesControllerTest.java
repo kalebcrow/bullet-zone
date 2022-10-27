42705be3b635534eef32bfe61b2da135192b7f87
kcrow <kcrowther32@gmail.com>
2022-10-27 00:56:09 -0400

    update GamesControllerTest

--- server/src/test/java/edu/unh/cs/cs619/bulletzone/web/GamesControllerTest.java
+++ server/src/test/java/edu/unh/cs/cs619/bulletzone/web/GamesControllerTest.java
@@ -6,19 +6,25 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import edu.unh.cs.cs619.bulletzone.BulletZoneServer;
+import edu.unh.cs.cs619.bulletzone.model.Tank;
 import edu.unh.cs.cs619.bulletzone.repository.InMemoryGameRepository;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -26,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class GamesControllerTest {
 
     MockHttpServletRequest request;
+
     MockMvc mockMvc;
 
 
@@ -47,39 +54,32 @@ public class GamesControllerTest {
 
 
     @Test
-    public void testCreateGame() throws Exception {
-        /*
-        repo = new InMemoryGameRepository();
-        gamesController = new GamesController(repo);
-        //String s = mockMvc.getDispatcherServlet().toString();
-        //gamesController = new GamesController(repo);
-        request = new MockHttpServletRequest();
-        request.setRemoteAddr("100.20.10.0");
-        //gamesController.join(request);
-         */
-        mockMvc.perform(post("/games").with(remoteAddr("100.20.10.0"))).andExpect(status().isCreated());
-
-
-    }
-
-    @Test
     public void testJoinGame() throws Exception {
-       // mockMvc.perform(post("/games")).andExpect(status().isCreated());
+        //joins game and returns tank id
+        mockMvc.perform(post("/games").with(remoteAddr("100.20.10.0"))).andExpect(status().isCreated());
     }
 
     @Test
     public void testGetGrid() throws Exception {
-       // mockMvc.perform(get("/games")).andExpect(status().isOk());
+        //performs get request on server gets ok response back
+       //MvcResult result = mockMvc.perform(get("/games").accept(MediaType.APPLICATION_JSON_VALUE).with(remoteAddr("100.20.10.0"))).andExpect(status().isOk());
+       //String tank = result.getResponse().toString();
     }
 
     @Test
     public void testLeaveGame() throws Exception {
-
+        //tests leave when tank is not present
+        mockMvc.perform(delete("/0/leave")).andExpect(status().isNotFound());
+        //adds tank
+        mockMvc.perform(post("/games").with(remoteAddr("100.20.10.0")));
+        //checks tank was deleted
+        mockMvc.perform(delete("/{id}/leave",0)).andExpect(status().isOk());
     }
     /*
     @Test
     public void test
     */
+
     private static RequestPostProcessor remoteAddr(final String remoteAddr) { // it's nice to extract into a helper
         return new RequestPostProcessor() {
             @Override
