a81dbdecdde767cbc7932067b59cc435dc4ff20a
keithbadger <keithbadger699@gmail.com>
2022-10-27 22:59:36 -0400

    updated events to fit new pattern

--- server/src/main/java/edu/unh/cs/cs619/bulletzone/model/events/DestroyWallEvent.java
+++ server/src/main/java/edu/unh/cs/cs619/bulletzone/model/events/DestroyWallEvent.java
@@ -2,49 +2,8 @@ package edu.unh.cs.cs619.bulletzone.model.events;
 
 public class DestroyWallEvent extends GridEvent{
 
-    private int row;
-    private int col;
-    private Long tankID;
-
-    public int getRow() {
-        return row;
-    }
-
-    public void setRow(int row) {
-        this.row = row;
-    }
-
-    public int getCol() {
-        return col;
-    }
-
-    public void setCol(int col) {
-        this.col = col;
-    }
-
-    public Long getTankID() {
-        return tankID;
-    }
-
-    public void setTankID(Long tankID) {
-        this.tankID = tankID;
-    }
-
-    public int getBulletID() {
-        return bulletID;
-    }
-
-    public void setBulletID(int bulletID) {
-        this.bulletID = bulletID;
-    }
-
-    private int bulletID;
-
-    public DestroyWallEvent(int row, int col, Long tankID, int bulletID) {
-        this.row = row;
-        this.col = col;
-        this.tankID = tankID;
-        this.bulletID = bulletID;
+    public DestroyWallEvent(int row, int col) {
+        this.pos = 16*row + col;
         this.type = "destroyWall";
         this.time = System.currentTimeMillis();
     }
--- server/src/main/java/edu/unh/cs/cs619/bulletzone/model/events/FireEvent.java
+++ server/src/main/java/edu/unh/cs/cs619/bulletzone/model/events/FireEvent.java
@@ -4,37 +4,8 @@ import edu.unh.cs.cs619.bulletzone.model.events.GridEvent;
 
 public class FireEvent extends GridEvent {
 
-    private Long tankID;
-    private int bulletID;
-    private byte direction;
-
-    public Long getTankID() {
-        return tankID;
-    }
-
-    public void setTankID(Long tankID) {
-        this.tankID = tankID;
-    }
-
-    public int getBulletID() {
-        return bulletID;
-    }
-
-    public void setBulletID(int bulletID) {
-        this.bulletID = bulletID;
-    }
-
-    public int getDirection() {
-        return direction;
-    }
-
-    public void setDirection(byte direction) {
-        this.direction = direction;
-    }
-
     public FireEvent(Long tankID, int bulletID, byte direction) {
-        this.tankID = tankID;
-        this.bulletID = bulletID;
+        this.ID = Math.toIntExact(tankID)*10 + bulletID;
         this.direction = direction;
         this.type = "fire";
         this.time = System.currentTimeMillis();
