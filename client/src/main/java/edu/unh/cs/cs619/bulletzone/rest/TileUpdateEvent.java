b3b666acd7705e40dbe6c0bd579ae04b85f58c2f
Sara <sko1006@usnh.edu>
2022-11-14 08:50:58 -0500

    removed obsolete variable

--- client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/TileUpdateEvent.java
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/TileUpdateEvent.java
@@ -1,16 +1,15 @@
 package edu.unh.cs.cs619.bulletzone.rest;
 
-
-import edu.unh.cs.cs619.bulletzone.game.tiles.BlankTile;
+import edu.unh.cs.cs619.bulletzone.game.tiles.GroundTile;
 
 /**
  * Created by simon on 10/3/14.
  */
 public class TileUpdateEvent {
     public Integer location;
-    public BlankTile movedTile;
+    public GroundTile movedTile;
 
-    public TileUpdateEvent(Integer location, BlankTile movedTile) {
+    public TileUpdateEvent(Integer location, GroundTile movedTile) {
         this.location = location;
         this.movedTile = movedTile;
     }
