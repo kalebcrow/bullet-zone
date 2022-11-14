e2455c83c971a2d70a42bc21ccb9f87eb2bd2d45
Sara <sko1006@usnh.edu>
2022-11-14 08:48:14 -0500

    updated tiles to account for terrain values, mostly just ground tile

--- client/src/main/java/edu/unh/cs/cs619/bulletzone/game/tiles/GroundTile.java
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/game/tiles/GroundTile.java
@@ -1,7 +1,7 @@
 package edu.unh.cs.cs619.bulletzone.game.tiles;
 
 import edu.unh.cs.cs619.bulletzone.R;
-public class BlankTile {
+public class GroundTile {
 
     /**
      *
@@ -12,6 +12,7 @@ public class BlankTile {
     }
 
     public Integer jsonValue = -1;
+    public Integer terrainJsonValue = -1;
 
     /**
      *
@@ -71,25 +72,68 @@ public class BlankTile {
     /**
      * make the tile
      */
-    public BlankTile() {
-        this.resourceID = R.drawable.rocky;
+    public GroundTile() {
+        // default meadow tile with json 0
+        this.resourceID = R.drawable.meadow;
         this.location = 0;
         this.jsonValue = 0;
     }
 
     /**
+     * Initializes the ground tile.
      *
      * @param JsonValue Value being used from server
      * @param location location
      */
-    public BlankTile(Integer JsonValue, Integer location) {
+    public GroundTile(Integer JsonValue, Integer location) {
+        // same as blank tile initialization
         this.resourceID = R.drawable.blank; // SARA
         this.location = location;
         this.jsonValue = JsonValue;
+        this.terrainJsonValue = JsonValue;
         orientation = 0;
         int row = location / 16 + 1;
         int column = location % 16 + 1;
         String r = String.valueOf(row);
         String c = String.valueOf(column);
+
+        // using given json values
+        if (JsonValue == 2) {
+            this.resourceID = R.drawable.hilly;
+        } else if (JsonValue == 1) {
+            this.resourceID = R.drawable.rocky;
+        } else if (JsonValue == 0) {
+            this.resourceID = R.drawable.meadow;// somethng is wrong SARA
+        }
+    }
+
+    public int getTerrain() {
+        // using given json values
+        if (terrainJsonValue == 2) {
+            return R.drawable.hilly;
+        } else if (terrainJsonValue == 1) {
+            return R.drawable.rocky;
+        } else if (terrainJsonValue == 0) {
+            return R.drawable.meadow;
+        }
+        // not a terrain basically
+        return R.drawable.blank;
     }
 }
+
+/*
+package edu.unh.cs.cs619.bulletzone.game.tiles;
+
+import edu.unh.cs.cs619.bulletzone.R;
+
+public class GroundTile extends BlankTile {
+
+    /**
+     * Initializes the ground tile.
+     *
+     * @param JsonValue value from server
+     * @param location location
+
+}
+
+ */
