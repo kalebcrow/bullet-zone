c29c8e6c5d70959a4fa45e61d8545d2abc4fe407
cgladu <cgladu@iol.unh.edu>
2022-11-19 17:45:45 -0500

    Finish testing for history

--- client/src/test/java/com/example/bulletzone/HistoryTest.java
+++ client/src/test/java/com/example/bulletzone/HistoryTest.java
@@ -1,42 +1,73 @@
 package com.example.bulletzone;
 
-import com.squareup.otto.Bus;
+import static org.mockito.Mockito.verify;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.gson.Gson;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
-import edu.unh.cs.cs619.bulletzone.game.BoardView;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+
+import edu.unh.cs.cs619.bulletzone.game.events.GridEvent;
 import edu.unh.cs.cs619.bulletzone.replay.HistoryInterpreter;
 import edu.unh.cs.cs619.bulletzone.replay.HistoryReader;
 import edu.unh.cs.cs619.bulletzone.replay.HistoryWriter;
 
 public class HistoryTest {
 
-    @Mock
-    HistoryInterpreter interpret;
-    @Mock
-    HistoryWriter write;
-    @Mock
-    HistoryReader read;
+    private static HistoryWriter writerTesting;
+    private static HistoryReader readerTesting;
 
-    BoardView testBoardView;
     @Mock
-    Bus mBus;
-
-    @BeforeClass
-    public void setup() {
-        interpret = new HistoryInterpreter();
-    }
-
-
+    OutputStreamWriter mockOutputStreamWriter;
+    OutputStreamWriter mockOutputStreamWriter2;
 
     @Test
-    public void historyGetsCorrectHistory()
-    {
+    public void historyGetsCorrectHistory() {
+        mockOutputStreamWriter = Mockito.mock((OutputStreamWriter.class));
+        mockOutputStreamWriter2 = Mockito.mock(OutputStreamWriter.class);
+        int[][][] array = new int[16][16][3];
+        int value = 0;
+        for (int i = 0; i < 16; i++) {
+            for (int ii = 0; ii < 16; ii++) {
+                for (int iii= 0; iii < 3; iii++) {
+                    array[i][ii][iii] = value;
+                    value++;
+                }
+            }
+        }
 
+        LinkedList<GridEvent> events = new LinkedList<>();
+
+        writerTesting = new HistoryWriter(events, array);
+
+        Gson gson = new Gson();
+        writerTesting.WriteArray(mockOutputStreamWriter);
+        try {
+            verify(mockOutputStreamWriter).write(gson.toJson(array));
+            verify(mockOutputStreamWriter).close();
+
+        }
+        catch (IOException e) {
+
+        }
+
+        writerTesting.WriteHistory(mockOutputStreamWriter2);
+        try {
+            verify(mockOutputStreamWriter2).write(gson.toJson(events));
+            verify(mockOutputStreamWriter2).close();
+
+        }
+        catch (IOException e) {
+
+        }
     }
-
-    @Test
-
 }
