f29415e6f41a06de064000d393badfb5c0ac26a7
kcrow <kcrowther32@gmail.com>
2022-12-03 17:22:44 -0500

    tank constraints for movement across water

--- client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/BulletZoneRestClient.java
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/BulletZoneRestClient.java
@@ -28,7 +28,7 @@ import edu.unh.cs.cs619.bulletzone.util.LongWrapper;
 //Please fix when we submit
 //@Rest(rootUrl = "http://10.21.133.147:8080/games",
 //@Rest(rootUrl = "http://10.21.95.176:8080/games",
-//@Rest(rootUrl = "http://10.21.95.176:8080/games",
+//@Rest(rootUrl = "http://10.21.125.49:8080/games",
 //@Rest(rootUrl = "http://stman1.cs.unh.edu:6192/games",
 //@Rest(rootUrl = "http://10.21.99.8:8080/games",
 @Rest(rootUrl = "http://stman1.cs.unh.edu:61902/games",
@@ -86,7 +86,7 @@ public interface BulletZoneRestClient extends RestClientErrorHandling {
     @Put("/{tankId}/dismantle")
     BooleanWrapper dismantle(@Path long tankId);
 
-    @Put("/{tankId]/rebuild")
+    @Put("/{tankId}/rebuild")
     BooleanWrapper rebuild(@Path long tankId);
 
     @Put("/{tankId}/build/{buildingType}")
