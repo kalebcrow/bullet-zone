7db32fa930a5f37b1aa63d8474b8a26aedacbe85
cgladu <cgladu@iol.unh.edu>
2022-12-10 13:30:55 -0500

    Adjussted some events

--- client/src/main/java/edu/unh/cs/cs619/bulletzone/game/events/MoveTankEvent.java
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/game/events/MoveTankEvent.java
@@ -24,83 +24,10 @@ public class MoveTankEvent extends ExecutableEvent {
         if (tile == null) {
             return;
         }
-
         Integer location = tile.getLocation();
-        Integer prevlocation = location;
+        tile.setLocation(this.pos + 1);
 
-        if (direction == 0) {
-            location = goingUp(location);
-        } else if (direction == 2) {
-            location = goingRight(location);
-        } else if (direction == 4) {
-            location = goingDown(location);
-        } else if (direction == 6) {
-            location = goingLeft(location);
-        }
-        tile.setLocation(location);
-
-        Log.d("Moving: ", "TankID: " + tile.ID + " location: " + tile.location);
-
-
-        tileUpdateEvent = new TileUpdateEvent(location, tile);
-        bus.post(tileUpdateEvent);
-        bus.post(new TileUpdateEvent(prevlocation, new GroundTile(-1, location)));
+        bus.post(new TileUpdateEvent(location, new GroundTile(5, location)));
+        bus.post(new TileUpdateEvent(this.pos+1, tile));
     }
-
-    /**
-     *
-     * @param location starting location
-     * @return ending location
-     */
-    private Integer goingUp(Integer location) {
-        if (location <= 15) {
-            location = 240 + location;
-        } else {
-            location = location - 16;
-        }
-        return location;
-    }
-
-    /**
-     *
-     * @param location starting location
-     * @return ending location
-     */
-    private Integer goingDown(Integer location) {
-        if (location >= 240) {
-            location = location - 240;
-        } else {
-            location = location + 16;
-        }
-        return location;
-    }
-
-    /**
-     *
-     * @param location starting location
-     * @return ending location
-     */
-    private Integer goingRight(Integer location) {
-        if ((location + 1) % 16 == 0) {
-            location = location - 15;
-        } else {
-            location = location + 1;
-        }
-        return location;
-    }
-
-    /**
-     *
-     * @param location starting location
-     * @return ending location
-     */
-    private Integer goingLeft(Integer location) {
-        if (location % 16 == 0) {
-            location = location + 15;
-        } else {
-            location = location - 1;
-        }
-        return location;
-    }
-
 }
